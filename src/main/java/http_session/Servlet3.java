85982ca88670b8ef7128c5c85879b7cb4c394759
TusharSupanekar <tsupanekar2810@gmail.com>
2023-10-08 23:19:04 +0530

    Http session

---
+++ src/main/java/http_session/Servlet3.java
@@ -0,0 +1,25 @@
+package http_session;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/servlet3")
+public class Servlet3 extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession httpSession = req.getSession();
+		String pwd = (String) httpSession.getAttribute("sessionpassword");
+		System.out.println(pwd);
+		
+		httpSession.invalidate();
+		
+	}
+
+}
